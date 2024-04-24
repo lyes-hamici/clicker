022cdac4138f64e9433ea504cbf7c952330ce10e
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-04-24 14:47:57 +0200

    <feat>[script.js]:<added localstorage>

--- script.js
+++ script.js
@@ -34,28 +34,51 @@ class Item {
   get Description() {
     return this.#description;
   }
+
+  toJSON() {
+    return {
+      name: this.#name,
+      price: this.#price,
+      possess: this.#possess,
+      imagePath: this.#imagePath,
+      description: this.#description,
+    };
+  }
 }
 
-
-
 document.addEventListener("DOMContentLoaded", function () {
-  var valueClick = 1;
-  var valueAuto = 0;
-  var score = 0;
+  var valueClick = localStorage.getItem("valueClick") || 1;
+  var valueAuto = localStorage.getItem("valueAuto") || 0;
+  var score = localStorage.getItem("score") || 0;
 
-  // Remplacer document par la div qui contient les items
-  document.addEventListener("click", addToScore)
+  var itemList =
+    JSON.parse(localStorage.getItem("itemList")) ||
+    [
+      // Exemple new Item("Cursor", 10, "images/cookie.png", "Auto clicker"),
+    ];
 
-  itemList = [
-    // Exemple new Item("Cursor", 10, "images/cookie.png", "Auto clicker"),
-  ];
-  
+  itemList = itemList.map(
+    (item) => new Item(item.name, item.price, item.imagePath, item.description)
+  );
+
+  // document should be changed to the id of the div
+  //   document.getElementById("")
+  document.addEventListener("click", addToScore);
 
   setInterval(function () {
     score += valueAuto;
+    localStorage.setItem("score", score);
   }, 1000);
 
   function addToScore() {
-    score += valueClick
+    score += valueClick;
+    localStorage.setItem("score", score);
   }
+
+//   Update local storage info every time the user leaves the page
+  window.addEventListener("beforeunload", function () {
+    localStorage.setItem("valueClick", valueClick);
+    localStorage.setItem("valueAuto", valueAuto);
+    localStorage.setItem("itemList", JSON.stringify(itemList));
+  });
 });
